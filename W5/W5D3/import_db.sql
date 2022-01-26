c33efacd2c1f4a8a484add21e078c95aebf81c74
Justin Nguyen <judnguyen@ucdavis.edu>
2022-01-26 10:59:52 -0800

    Create tables

--- W5/W5D3/import_db.sql
+++ W5/W5D3/import_db.sql
@@ -5,3 +5,53 @@ CREATE TABLE users (
   f_name TEXT NOT NULL,
   l_name TEXT NOT NULL,
 );
+---------------------------------
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+---------------------------------
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+---------------------------------
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
+);
+
+---------------------------------
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  likes BOOLEAN NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
