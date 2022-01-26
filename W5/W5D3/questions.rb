4e2714d2747b3e9d5e27d4de08f115ada8f50290
anahmmacher <acnahmmacher@gmail.com>
2022-01-26 12:22:31 -0800

    Adding author method

--- W5/W5D3/questions.rb
+++ W5/W5D3/questions.rb
@@ -76,3 +76,33 @@ class User
         SQL
     end
 end
+
+class Question 
+
+    attr_accessor :id, :title, :body, :author_id
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM questions")
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def self.find_by_author_id(author_id)
+        question = QuestionsDBConnection.instance.execute(<<-SQL, author_id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                author_id = ?
+        SQL
+        return nil unless question.length > 0
+
+        Question.new(question.first)
+    end
+
+    def author
+        User.find_by_id(author_id)
+    end
+
+
+end
