c9b961837964ea0b86aa44371020ec8bba0a02b1
anahmmacher <acnahmmacher@gmail.com>
2022-02-02 15:42:37 -0800

    adding destroy

--- W6/W6D3/controller/app/controllers/users_controller.rb
+++ W6/W6D3/controller/app/controllers/users_controller.rb
@@ -9,11 +9,37 @@ class UsersController < ApplicationController
   def create
   user = User.new(params.require(:user).permit(:name, :email))
   # replace the `user_attributes_here` with the actual attribute keys
-  user.save!
-  render json: user
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to "/user/#{user.id}"
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+        render json: 'User was destroyed'
+    else
+        render json: user.errors.full_messages, status: 422
+    end
+end
+
+  def user_params
+    params.require(:user).permit(:username, :age, :email, :coding_affiliation)
   end
 end
